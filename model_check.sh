bda94df021b6ee1fd1a98c7710c0cf861fd7c6d2
GYG <gyg@smail.nju.edu.cn>
2022-10-19 16:46:30 +0800

    add SKEE and SelMon

--- model_check.sh
+++ model_check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 probcli_path=/usr/software/ProB/probcli
+#probcli_path=~/Documents/App/ProB/probcli
 model_check="-model-check -mc-mode bf -p COMPRESSION TRUE -p DOT_IDS TRUE -memory -disable-timeout --coverage-summary"
 m2dot="-dot state_space_sfdp"
 dot_path=/usr/bin/dot
@@ -8,7 +9,7 @@ dot2pdf="-Ksfdp -x -Goverlap=scale -Tpdf"
 echo "Generate state spaces? (y or n)"
 read space
 echo "Please select the model to check:"
-echo "input \"n\" for Nested Kernel, \"h\" for Hilps, or \"all\""
+echo "input \"n\" for Nested Kernel, \"h\" for Hilps, \"sk7\" for SKEE_ARMv7, \"sk8\" for SKEE_ARMv8, \"sm\" for SelMon, or \"all\""
 read model
 if [ $space = "y" ] || [ $space = "Y" ];then
     case $model in
@@ -24,6 +25,24 @@ if [ $space = "y" ] || [ $space = "Y" ];then
             echo "Generating the state space..."
             $dot_path $dot2pdf ./Hilps/Hilps.dot -o ./Hilps/Hilps.pdf
             ;;
+        "sk7")
+            echo "Start SKEE_ARMv7 model checking..."
+            $probcli_path ./SKEE/Priv_Domain_SKEEv7.mch $model_check $m2dot ./SKEE/SKEEv7.dot
+            echo "Generating the state space..."
+            $dot_path $dot2pdf ./SKEE/SKEEv7.dot -o ./SKEE/SKEEv7.pdf
+            ;;
+        "sk8")
+            echo "Start SKEE_ARMv8 model checking..."
+            $probcli_path ./SKEE/Priv_Domain_SKEEv8.mch $model_check $m2dot ./SKEE/SKEEv8.dot
+            echo "Generating the state space..."
+            $dot_path $dot2pdf ./SKEE/SKEEv8.dot -o ./SKEE/SKEEv8.pdf
+            ;;
+        "sm")
+            echo "Start SelMon model checking..."
+            $probcli_path ./SelMon/Priv_Domain_SelMon.mch $model_check $m2dot ./SelMon/SelMon.dot
+            echo "Generating the state space..."
+            $dot_path $dot2pdf ./SelMon/SelMon.dot -o ./SelMon/SelMon.pdf
+            ;;
         "all")
             echo "Start Nested Kernel model checking..."
             echo "Nested Kernel" > model_check.txt
@@ -35,6 +54,21 @@ if [ $space = "y" ] || [ $space = "Y" ];then
             $probcli_path ./Hilps/Priv_Domain_Hilps.mch $model_check $m2dot ./Hilps/Hilps.dot >> model_check.txt
             echo "Generating the state space..."
             $dot_path $dot2pdf ./Hilps/Hilps.dot -o ./Hilps/Hilps.pdf
+            echo "Start SKEE_ARMv7 model checking..."
+            echo "SKEE_ARMv7" >> model_check.txt
+            $probcli_path ./SKEE/Priv_Domain_SKEEv7.mch $model_check $m2dot ./SKEE/SKEEv7.dot >> model_check.txt
+            echo "Generating the state space..."
+            $dot_path $dot2pdf ./SKEE/SKEEv7.dot -o ./SKEE/SKEEv7.pdf
+            echo "Start SKEE_ARMv8 model checking..."
+            echo "SKEE_ARMv8" >> model_check.txt
+            $probcli_path ./SKEE/Priv_Domain_SKEEv8.mch $model_check $m2dot ./SKEE/SKEEv8.dot >> model_check.txt
+            echo "Generating the state space..."
+            $dot_path $dot2pdf ./SKEE/SKEEv8.dot -o ./SKEE/SKEEv8.pdf
+            echo "Start SelMon model checking..."
+            echo "SelMon" >> model_check.txt
+            $probcli_path ./SelMon/Priv_Domain_SelMon.mch $model_check $m2dot ./SelMon/SelMon.dot >> model_check.txt
+            echo "Generating the state space..."
+            $dot_path $dot2pdf ./SelMon/SelMon.dot -o ./SelMon/SelMon.pdf
             ;;
         *)
             echo "Wrong parameters!"
@@ -50,6 +84,18 @@ else
             echo "Start Hilps model checking..."
             $probcli_path ./Hilps/Priv_Domain_Hilps.mch $model_check
             ;;
+        "sk7")
+            echo "Start SKEE_ARMv7 model checking..."
+            $probcli_path ./SKEE/Priv_Domain_SKEEv7.mch $model_check
+            ;;
+        "sk8")
+            echo "Start SKEE_ARMv8 model checking..."
+            $probcli_path ./SKEE/Priv_Domain_SKEEv8.mch $model_check
+            ;;
+        "sm")
+            echo "Start SelMon model checking..."
+            $probcli_path ./SelMon/Priv_Domain_SelMon.mch $model_check
+            ;;
         "all")
             echo "Start Nested Kernel model checking..."
             echo "Nested Kernel" > model_check.txt
@@ -57,6 +103,15 @@ else
             echo "Start Hilps model checking..."
             echo "Hilps" >> model_check.txt
             $probcli_path ./Hilps/Priv_Domain_Hilps.mch $model_check >> model_check.txt
+            echo "Start SKEE_ARMv7 model checking..."
+            echo "SKEE_ARMv7" >> model_check.txt
+            $probcli_path ./SKEE/Priv_Domain_SKEEv7.mch $model_check >> model_check.txt
+            echo "Start SKEE_ARMv8 model checking..."
+            echo "SKEE_ARMv8" >> model_check.txt
+            $probcli_path ./SKEE/Priv_Domain_SKEEv8.mch $model_check >> model_check.txt
+            echo "Start SelMon model checking..."
+            echo "SelMon" >> model_check.txt
+            $probcli_path ./SelMon/Priv_Domain_SelMon.mch $model_check >> model_check.txt
             ;;
         *)
             echo "Wrong parameters!"
